95e5ec684fc4d1453a48afc577ec9d5848d1f2eb
unknown <vankudregoutam15@gmail.com>
2023-03-15 00:25:11 +0530

    second commit

--- backend/App.js
+++ backend/App.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const Swal = require('sweetalert2');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const mongoURL = 'mongodb+srv://goutamv1503:pArTh1503@cluster0.bz1zhhr.mongodb.net/?retryWrites=true&w=majority';
 
 // using this we can easily find the data is tempered or not and if done we can get to know by using the 3rd part that is returned by authToken
 const JWT_SECRET = 'BlockchainbasedeVoting'
